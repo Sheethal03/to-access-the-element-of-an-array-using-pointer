1a63ccd8a17c79e2b08d2687cb7c446e835b3bdb
Sheethal03 <90698417+Sheethal03@users.noreply.github.com>
2021-10-16 00:20:34 +0530

    created by cpp

---
+++ stdio.h
@@ -0,0 +1,10 @@
+#include(stdio.h)
+#int main()
+{
+   intx[4]={2,3, 5,4]; i;
+      printf("accessing array elements using subscript\n");
+   for(i=0; i<=3; i++)  print("%d",x[i]);
+       printf("access array elements using just array name\n");
+   for (i=0; i<=3; i++)  print("%d",x(+i)); 
+ return 0;
+}
